40cd6ebced5ea833af3cc9a70303d3c9196e0e4d
luizinbrzado <luiztrineves@gmail.com>
2021-10-27 13:39:02 -0300

    Estilizando: aula 06 - mesa

--- Mesa/aula-06/src/container/App.js
+++ Mesa/aula-06/src/container/App.js
@@ -5,11 +5,11 @@ import '../styles/App.css';
 function App() {
   return (
     <div className="App">
-      <h3>Convidado:</h3>
+      <h3>Convidados:</h3>
       <ClassComponent nome="Nicolas" estaNaLista={true} />
       <ClassComponent nome="Pedro" estaNaLista={false} />
       <ClassComponent nome="Carolina" estaNaLista={true} />
-      <h3>Tarefas: </h3>
+      <h3>Tarefas:</h3>
       <FunctionComponent nome="Nicolas" tarefa="batata-frita" />
       <FunctionComponent nome="Pedro" tarefa="pizza" />
       <FunctionComponent nome="Carolina" tarefa="bebidas" />
