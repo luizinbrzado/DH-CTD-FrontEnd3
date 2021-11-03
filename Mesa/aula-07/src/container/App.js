ac6ded18cae272d6aab3370d97e1a683a7659b9a
luizinbrzado <luiztrineves@gmail.com>
2021-11-03 11:59:55 -0300

    Aula 07 - Mesa

---
+++ Mesa/aula-07/src/container/App.js
@@ -0,0 +1,55 @@
+import ClassComponent from '../components/ClassComponent';
+import FunctionComponent from '../components/FunctionComponent';
+import '../styles/App.css';
+
+function App() {
+  const lista = [
+    {
+      nome: 'Nicolas',
+      estaNaLista: true,
+      tarefa: 'batata-frita'
+    },
+    {
+      nome: 'Pedro',
+      estaNaLista: false,
+      tarefa: 'pizza'
+    },
+    {
+      nome: 'Carolina',
+      estaNaLista: true,
+      tarefa: 'bebidas'
+    }
+
+  ]
+
+  return (
+
+    <div className="App">
+      <h3>Convidados:</h3>
+
+      {lista.map((nomes, index) => {
+        return (
+          <>
+            <div className="container">
+              <ClassComponent key={index} nome={nomes.nome} estaNaLista={nomes.estaNaLista} />
+            </div>
+          </>
+        )
+      })}
+
+      <h3>Tarefa: </h3>
+
+      {lista.map((nomes, index) => {
+        return (
+          <>
+            <div className="container">
+              <FunctionComponent key={index} nome={nomes.nome} tarefa={nomes.tarefa} />
+            </div>
+          </>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
