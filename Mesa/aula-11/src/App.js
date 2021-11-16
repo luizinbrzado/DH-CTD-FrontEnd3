fedd9e583b331c8d2a8f7e4fcc8e2c4002f34175
luizinbrzado <luiztrineves@gmail.com>
2021-11-16 12:00:32 -0300

    Mesa aula 11

---
+++ Mesa/aula-11/src/App.js
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import { Formik, Field, Form } from 'formik';
+import './App.css';
+import Swal from 'sweetalert2';
+
+export default class App extends Component {
+
+  handleSubmit = (values) => {
+    console.log(values);
+    Swal.fire({
+      title: 'Valores enviados',
+      text: 'Sucesso, tudo funcionou!',
+      icon: 'success',
+      html: `Nome: ${values.nome}<br>
+      Telefone: ${values.telefone}<br>
+      Email: ${values.email}`,
+      confirmButtonText: 'Nice'
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Formik
+          initialValues={{
+            nome: '',
+            telefone: '',
+            idade: 0
+          }}
+          onSubmit={this.handleSubmit}>
+          <Form>
+            <label for="nome">Nome</label>
+            <Field type={'text'} className="form-control" id="nome" name="nome" placeholder="Digite seu nome" />
+            
+            <label for="telefone">Telefone</label>
+            <Field type={'text'} className="form-control" id="telefone" name="telefone" placeholder="Digite seu telefone" />
+
+            <label for="email">Email</label>
+            <Field type={"text"} className="form-control" id="email" name="email" placeholder="Digite sua email" />
+            
+            <button className="btn my-3 btn-primary" type="submit">Enviar</button>
+          </Form>
+        </Formik>
+      </>
+    );
+  }
+}
